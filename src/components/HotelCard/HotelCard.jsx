b7fdb7568cc47d4a3b66c5ee3a078944931212a1
sunny <sunny129271@gmail.com>
2024-08-08 22:03:43 +0530

    added hotelcards

--- src/components/HotelCard/HotelCard.jsx
+++ src/components/HotelCard/HotelCard.jsx
@@ -1,27 +1,26 @@
 import React from "react"
 import "./HotelCard.css"
 
-export const HotelCard = () => {
+export const HotelCard = ({ hotel }) => {
   return (
     <>
       <div className="relative hotelcard-container shadow cursor-pointer">
         <div>
-          <img
-            className="img"
-            src="https://images.unsplash.com/photo-1694964363999-964f056854ae?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-          />
+          <img className="img" src={hotel.image} alt={hotel.name} />
           <div className="hotelcard-details">
             <div className="d-flex align-center">
-              <span className="location">Bir , Himachal Pradesh</span>
+              <span className="location">
+                {hotel.address}{` `}
+                {hotel.state}
+              </span>
               <span className="rating d-flex align-center">
                 <span class="material-icons-outlined">star</span>
-                <span>4.5</span>
+                <span>{hotel.rating}</span>
               </span>
             </div>
-            <p className="hotel-name">Sukoon bag</p>
+            <p className="hotel-name">{hotel.name}</p>
             <p className="price-details">
-              <span className="price">Rs. 5300</span>
+              <span className="price">Rs. {hotel.price}</span>
               <span>night</span>
             </p>
           </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,28 @@
-import React from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
+import InfiniteScroll from "react-infinite-scroll-component"
 import { Navbar, HotelCard } from "../../components"
 import "./Home.css"
 
 export const Home = () => {
+  const [hotels, setHotels] = useState([])
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const { data } = await axios.get("http://localhost:4000/api/hotels")
+        setHotels(data)
+        // console.log(hotels)
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+  }, [])
   return (
     <>
       <Navbar />
-      <main className="main">
-        <HotelCard />
+      <main className="main d-flex wrap align-centre gap-larger">
+        {hotels &&
+          hotels.map((hotel) => <HotelCard key={hotel._id} hotel={hotel} />)}
       </main>
     </>
   )
